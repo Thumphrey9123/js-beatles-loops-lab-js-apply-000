76e7976313c5af0dbf2beefebfadedf056ef8c14
Thomas Humphrey <thumphrey9123@gmail.com>
2019-11-12 20:07:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// add solution here
+function theBeatlesPlay(mus, inst){
+  var arr = []
+  for(let i=0;i>mus.length; i++){
+    arr.push(`${mus[i]} plays ${inst[i]}`)
+  }
+  return arr
+}
+
+function johnLennonFacts(facts){
+  
+}
